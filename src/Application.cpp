46e499bd4e6ee5e93a2cb36ef04e895747009775
alpha <alpha80000@gmail.com>
2013-08-30 20:27:55 +0200

    L'application est fonctionnelle

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include "Application.h"
 
 Application::Application() {
--- src/metal_slug.cpp
+++ src/metal_slug.cpp
@@ -1,10 +1,9 @@
 // metal_slug.cpp : définit le point d'entrée pour l'application console.
 //
 
-#include "stdafx.h"
 #include "Application.h"
 
-int _tmain(int argc, _TCHAR* argv[])
+int main()
 {
 	//Le main très court
 	Application app;

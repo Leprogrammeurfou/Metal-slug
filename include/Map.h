9844d419109236aeedd7f6de798a967d97e55e14
alpha <alpha80000@gmail.com>
2013-08-31 02:59:49 +0200

    Création de la classe définissant la map

---
+++ include/Map.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <SFML\Graphics.hpp>
+#include "Parser.h"
+#include <cmath>
+//Un tile de la map
+class Tile : public sf::Sprite {
+private:
+	//Savoir si le tile est un obstacle
+	bool _obstacle;
+
+public:
+	Tile();
+	//Définir le tile comme un obstacle
+	void obstacle();
+	//Retourne true s'il y a une collision, prend en paramètre la boite englobante
+	bool collision(sf::IntRect boiteEnglobante);
+	//Retourne true si le tile est un obstacle
+	bool isObstacle();
+};
+
+class Map {
+private:
+	//Le tileset
+	sf::Texture tileset;
+	//Le tableau contenant les sprites de la map
+	std::vector < std::vector < Tile > > tableau;
+	//le nom de la map
+	std::string nom;
+	//la largeur, la hauteur de la map, la largeur d'un tile, la hauteur d'un tile
+	int largeur, hauteur, largeur_tile, hauteur_tile,nombre_tiles_largeur,nombre_tiles_hauteur;
+public:
+	//Charger la map
+	void charger(Parser& parser);
+	//Afficher la map
+	void afficher(sf::RenderWindow& app);
+	//Retourne le tile de la position demandée
+	sf::Vector2f getTile(sf::Vector2f position);
+	//Retourne le tile s'il y a collision
+	Tile* collision(sf::IntRect boundingBox);
+};
---
+++ src/Map.cpp
@@ -0,0 +1,113 @@
+#include "Map.h"
+
+bool in_array(std::string recherche,std::vector < std::string > tableau) {
+	for (int i = 0;i < tableau.size();i++)
+		if(tableau[i] == recherche)
+			return true;
+
+	return false;
+}
+
+Tile::Tile() : sf::Sprite(),_obstacle(false) {
+}
+
+void Tile::obstacle() {
+	_obstacle = true;
+}
+
+bool Tile::isObstacle() {
+	return _obstacle;
+}
+
+bool Tile::collision(sf::IntRect boundingBox) {
+	sf::IntRect myBoundingBox = sf::IntRect(getPosition().x,
+		getPosition().y,getTextureRect().width,getTextureRect().height);
+
+	return myBoundingBox.left + myBoundingBox.width > boundingBox.left && 
+		myBoundingBox.left < boundingBox.left + boundingBox.width && 
+		myBoundingBox.top + myBoundingBox.height > boundingBox.top &&
+		myBoundingBox.top < boundingBox.top + boundingBox.height;
+}
+
+void Map::charger(Parser& parser) {
+	//Chargement du tileset
+	std::string k = parser.get("tileset");
+	tileset.loadFromFile(parser.get("tileset"));
+
+	//On initialise les paramètres de la map
+	nom = parser.get("nom");
+	largeur = parser.geti("largeur");
+	hauteur = parser.geti("hauteur");
+	largeur_tile = parser.geti("largeur_tile");
+	hauteur_tile = parser.geti("hauteur_tile");
+	nombre_tiles_largeur = largeur/largeur_tile;
+	nombre_tiles_hauteur = hauteur/hauteur_tile;
+
+	//On récupère les obstacles
+	std::vector < std::string > tiles_obstacles = Parser::explode(',',parser.get("obstacles"));
+
+	//Ok on récupère les tiles
+	std::vector < std::string > tiles = Parser::explode(',',parser.get("tiles"));
+
+	//on rempli le tableau
+	for(int y = 0;y < nombre_tiles_hauteur;y++) {
+		std::vector < Tile > sprites;
+		for(int x = 0;x < nombre_tiles_largeur;x++) {
+			bool obstacle = in_array(tiles[y * nombre_tiles_largeur + x],tiles_obstacles);
+			//On récupère le numéro du tile
+			int tile = Parser::stringToInt(tiles[y * nombre_tiles_largeur + x]) - 1;
+			//On récupère la position du tile
+			int position_x = tile%int(tileset.getSize().x/largeur_tile);
+			double position_y = floor((double) tile/int(tileset.getSize().x/largeur_tile));
+
+			//Création du sprite
+			Tile sprite;
+			sprite.setTexture(tileset);
+			//Découpage du tileset
+			sprite.setTextureRect(sf::IntRect((position_x) * largeur_tile,(position_y) * hauteur_tile,largeur_tile,hauteur_tile));
+			sprite.setPosition(x * largeur_tile,y * hauteur_tile);
+
+			//Si c'est un obstacle, nous le signalons
+			if(obstacle)
+				sprite.obstacle();
+
+			sprites.push_back(sprite);
+		}
+		//On ajoute la ligne au tableau
+		tableau.push_back(sprites);
+	}
+}
+
+void Map::afficher(sf::RenderWindow& app) {
+	for(int i = 0;i < tableau.size();i++) {
+		for(int j = 0;j < tableau[i].size();j++) {
+			app.draw(tableau[i][j]);
+		}
+	}
+}
+
+sf::Vector2f Map::getTile(sf::Vector2f position) {
+	int x = position.x/largeur_tile, y = position.y/hauteur_tile;
+	return sf::Vector2f(x,y);
+}
+
+Tile* Map::collision(sf::IntRect boundingBox) {
+	sf::Vector2f tile = getTile(sf::Vector2f(boundingBox.left,boundingBox.top));
+	for(int j = -1;j < 2;j++) {
+		for(int i = -1;i < 2;i++) {
+			int x = tile.x + i,y = tile.y + j;
+			std::cout << x << ", "<< y << std::endl;
+			//Si la position existe
+			if(y >= 0 && y < tableau.size() && x >= 0 && y < tableau[y].size()) {
+
+				//Si c'est un obstacle et en collision
+				if(tableau[y][x].isObstacle()) {
+					if(tableau[y][x].collision(boundingBox))
+						return &tableau[y][x];
+				}
+			}
+		}
+	}
+
+	return NULL;
+}
